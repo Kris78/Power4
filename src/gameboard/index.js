b956e1513fc2ef49b41958b57e529e5f2676b049
Mohammed Ousama BELATAR <bel3atar@aol.com>
2019-03-22 12:05:49 +0100

    use columns

--- src/gameboard/index.js
+++ src/gameboard/index.js
@@ -2,21 +2,24 @@ import { h, Component } from 'splay'
 export default class GameBoard extends Component {
   init () {
     this.grid = [
-      Array(7).fill(null),
-      Array(7).fill(null),
-      Array(7).fill(null),
-      Array(7).fill(null),
-      Array(7).fill(null),
-      Array(7).fill(null)
+      Array(6).fill(null),
+      Array(6).fill(null),
+      Array(6).fill(null),
+      Array(6).fill(null),
+      Array(6).fill(null),
+      Array(6).fill(null),
+      Array(6).fill(null)
     ]
   }
   render () {
     return <div class='game-board'>
-      {this.grid.map(ligne =>
-        <div className='game-board__row'>
-          {ligne.map(pawn => <div className={`game-board__pawn ${pawn
+      {this.grid.map(column =>
+        <div className='game-board__column'>
+          {column.map(pawn => <div className={`game-board__pawn ${pawn
             ? 'game-board__pawn--p1'
-            : pawn === false ? 'game-board__pawn--p2' : ''}`}>
+            : pawn === false
+              ? 'game-board__pawn--p2'
+              : ''}`}>
           </div>)}
         </div>)}
     </div>
